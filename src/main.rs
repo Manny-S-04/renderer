30349e4bed4274dc2681014da8284e305d59dd5d
Manny <mansimran2001@googlemail.com>
2026-08-17 10:15:06 +0100

    can rotate points some shapes too

---
+++ src/main.rs
@@ -0,0 +1,332 @@
+use std::{thread::{self, sleep}, time::Duration};
+
+#[allow(unused)]
+use x11::xlib::*;
+
+const FRAMES: i32 = 30;
+
+#[derive(Copy, Clone, Debug)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+
+#[allow(unused)]
+impl Point {
+    pub fn mut_point(&mut self, x: f32, y: f32) {
+        self.x = x;
+        self.y = y;
+    }
+
+    pub fn to_polar(&self) -> (f32, f32) {
+        let radius = (self.x * self.x + self.y * self.y).sqrt();
+        let angle = self.y.atan2(self.x);
+        (radius, angle)
+    }
+
+    pub fn from_polar(radius: f32, angle: f32) -> Self {
+        Point {
+            x: radius * angle.cos(),
+            y: radius * angle.sin(),
+        }
+    }
+
+    pub fn rotate(&mut self, center: &Point, destination: &Point) {
+        let p = self;
+        let max_angle = angle(&p, &center, &destination);
+        let (radius, angle) = Point { x: p.x - center.x, y: p.y - center.y }.to_polar();
+        let step = max_angle / FRAMES as f32;
+        let mut curr_angle: f32 = angle + step;
+
+        let dx = radius * curr_angle.cos();
+        let dy = radius * curr_angle.sin();
+        let new_point = Point {
+            x: center.x + dx,
+            y: center.y + dy,
+        };
+        p.mut_point(new_point.x, new_point.y);
+    }
+}
+
+
+// todo:
+// think of how to redesign the api so its more ergonomic
+// draw multiple triangles
+// add a gradient
+// more shapes?
+// learn how to make a background persist
+// learn to draw an animated object on the background
+// user input, maybe here pause the screen no updates until user input
+// -> exit out of immediate mode
+
+fn main() {
+    let width = 800;
+    let height = 600;
+    let mut pixels: Vec<u32> = vec![0; width * height];
+    unsafe {
+        let (display, screen, root) = create_window();
+        let (window, gc) = create_graphics_context(display, root, width, height);
+        let image = create_image(display, screen, &mut pixels, width as u32, height as u32);
+
+        let d1 = point(400.0, 250.0);
+        let d2 = point(350.0, 350.0);
+        let d3 = point(450.0, 350.0);
+
+        let mut p1 = d1.clone();
+        let mut p2 = d2.clone();
+        let mut p3 = d3.clone();
+
+        let center = centroid(&p1, &p2, &p3);
+        loop {
+            pixels.fill(0xaabbcc);
+
+            //draw_circle(width, height, &mut pixels, 5, p1.x as i32, p1.y as i32, 0xffffff);
+            //draw_circle(width, height, &mut pixels, 5, p2.x as i32, p2.y as i32, 0xffffff);
+            //draw_circle(width, height, &mut pixels, 5, p3.x as i32, p3.y as i32, 0xffffff);
+            draw_triangle(width, height, &mut pixels, &p1, &p2, &p3, 0xffffff);
+            draw_circle(width, height, &mut pixels, 5, center.x as i32, center.y as i32, 0xff0000);
+
+            XPutImage(display, window, gc, image, 0, 0, 0, 0, width as u32, height as u32);
+            XFlush(display);
+
+            // i want a triangle object which captures these 3 points
+            // so i can do triangle.rotate(&center);
+            p1.rotate(&center, &p3);
+            p3.rotate(&center, &p2);
+            p2.rotate(&center, &p1);
+
+            sleep(Duration::from_millis((100 / FRAMES) as u64));
+        }
+    }
+}
+
+fn angle(p1: &Point, center: &Point, p3: &Point) -> f32 {
+    let v1 = (p1.x - center.x, p1.y - center.y);
+    let v3 = (p3.x - center.x, p3.y - center.y);
+    
+    let mag1 = (v1.0 * v1.0 + v1.1 * v1.1).sqrt();
+    let mag3 = (v3.0 * v3.0 + v3.1 * v3.1).sqrt();
+    let dot = v1.0 * v3.0 + v1.1 * v3.1;
+    
+    let cos_angle = (dot / (mag1 * mag3)).clamp(-1.0, 1.0);
+    
+    cos_angle.acos()
+}
+
+fn centroid(p1: &Point, p2: &Point, p3: &Point) -> Point {
+   return point((p1.x + p2.x + p3.x) / 3.0, (p1.y + p2.y + p3.y) / 3.0); 
+}
+
+/*
+
+let (astep_x, astep_y) = calc_step(&p1, &p3);
+let (bstep_x, bstep_y) = calc_step(&p3, &p2);
+let (cstep_x, cstep_y) = calc_step(&p2, &p1);
+
+if astep_x != 0.0 && astep_y != 0.0 {
+p1.mut_point(p1.x + astep_x, p1.y + astep_y);
+} else {
+p1.mut_point(b1.x, b1.y);
+}
+
+if bstep_x != 0.0 && bstep_y != 0.0 {
+p3.mut_point(p3.x + bstep_x, p3.y + bstep_y);
+} else {
+p3.mut_point(b3.x, b3.y);
+}
+
+if cstep_x != 0.0 && cstep_y != 0.0 {
+p2.mut_point(p2.x + cstep_x, p2.y + cstep_y);
+} else {
+p2.mut_point(b2.x, b2.y);
+}
+
+
+*/
+
+fn calc_step(pi: &Point, pf: &Point) -> (f32, f32) {
+    let dx = pf.x - pi.x;
+    let dy = pf.y - pi.y;
+
+    let step_x = dx / FRAMES as f32;
+    let step_y = dy / FRAMES as f32;
+
+    return (step_x, step_y);
+}
+
+fn draw_circle(width: usize, height: usize, pixels: &mut Vec<u32>, radius: i32, cx: i32, cy: i32, color: u32) {
+    let r2 = radius * radius;
+
+    let min_x = (cx - radius).max(0);
+    let max_x = (cx + radius).min(width as i32 - 1);
+    let min_y = (cy - radius).max(0);
+    let max_y = (cy + radius).min(height as i32 - 1);
+
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            let dx = x - cx;
+            let dy = y - cy;
+
+            if dx * dx + dy * dy <= r2 {
+                let idx = (y * width as i32 + x) as usize;
+                pixels[idx] = color;
+            }
+        }
+    }
+}
+
+fn point(x: f32, y: f32) -> Point {
+    Point{ x, y }
+}
+
+fn edge_function(a: &Point, b: &Point, c: &Point) -> f32 {
+    (c.x - a.x) * (b.y - a.y) - (c.y - a.y) * (b.x - a.x)
+}
+
+fn draw_triangle(
+    width: usize,
+    height: usize,
+    pixels: &mut [u32],
+    p0: &Point,
+    p1: &Point,
+    p2: &Point,
+    color: u32,
+) {
+    let min_x = p0.x.min(p1.x).min(p2.x).floor().max(0.0) as usize;
+    let max_x = p0.x.max(p1.x).max(p2.x).ceil().min(width as f32 - 1.0) as usize;
+    let min_y = p0.y.min(p1.y).min(p2.y).floor().max(0.0) as usize;
+    let max_y = p0.y.max(p1.y).max(p2.y).ceil().min(height as f32 - 1.0) as usize;
+
+    let area = edge_function(&p0, &p1, &p2);
+    if area == 0.0 {
+        return;
+    }
+
+    for y in min_y..=max_y {
+        for x in min_x..=max_x {
+            let p = Point {
+                x: x as f32 + 0.5,
+                y: y as f32 + 0.5,
+            };
+
+            let w0 = edge_function(&p1, &p2, &p);
+            let w1 = edge_function(&p2, &p0, &p);
+            let w2 = edge_function(&p0, &p1, &p);
+
+            let inside = (w0 >= 0.0 && w1 >= 0.0 && w2 >= 0.0)
+            || (w0 <= 0.0 && w1 <= 0.0 && w2 <= 0.0);
+
+            if inside {
+                let idx = y * width + x;
+                pixels[idx] = color;
+            }
+        }
+    }
+}
+
+fn draw_line(
+    width: usize,
+    height: usize,
+    pixels: &mut Vec<u32>,
+    (x1, y1): (i32, i32),
+    (x2, y2): (i32, i32),
+    color: u32,
+) {
+    let dx = (x2 - x1).abs();
+    let dy = -(y2 - y1).abs();
+    let sx = if x1 < x2 { 1 } else { -1 };
+    let sy = if y1 < y2 { 1 } else { -1 };
+    let mut err = dx + dy;
+
+    let mut x = x1;
+    let mut y = y1;
+
+    loop {
+        if x >= 0 && x < width as i32 && y >= 0 && y < height as i32 {
+            let idx = y as usize * width + x as usize;
+            pixels[idx] = color;
+        }
+
+        if x == x2 && y == y2 {
+            break;
+        }
+
+        let e2 = 2 * err;
+        if e2 >= dy {
+            err += dy;
+            x += sx;
+        }
+        if e2 <= dx {
+            err += dx;
+            y += sy;
+        }
+    }
+}
+
+#[allow(unused_variables)]
+#[allow(unused)]
+fn draw_checker(width: usize, height: usize, pixels: &mut Vec<u32>) {
+    for i in 0..width*height {
+        if (i % 2) == 0 {
+            pixels[i] = 0xff0000;
+            continue
+        }
+        pixels[i] = 0x0000ff;
+    }
+}
+
+unsafe fn create_window() -> (*mut _XDisplay, i32, u64) {
+    unsafe {
+        let display = XOpenDisplay(std::ptr::null());
+
+        if display.is_null() {
+            panic!("Cannot open X display");
+        }
+
+        let screen = XDefaultScreen(display);
+        let root = XRootWindow(display, screen);
+        (display, screen, root)
+    }
+}
+
+unsafe fn create_graphics_context(display: *mut _XDisplay, root: u64, width: usize, height: usize) -> (u64, *mut _XGC) {
+    unsafe {
+        let window = XCreateSimpleWindow(
+            display,
+            root,
+            0,
+            0,
+            width as u32,
+            height as u32,
+            1,
+            0,
+            0,
+        );
+
+        XMapWindow(display, window);
+        XFlush(display);
+
+        (window, XCreateGC(display, window, 0, std::ptr::null_mut()))
+    }
+}
+
+unsafe fn create_image(display: *mut _XDisplay, screen: i32, pixels: &mut Vec<u32>, width: u32, height: u32) -> *mut XImage {
+    unsafe {
+        let visual = XDefaultVisual(display, screen);
+        let depth = XDefaultDepth(display, screen);
+
+        XCreateImage(
+            display,
+            visual,
+            depth as u32,
+            ZPixmap,
+            0,
+            pixels.as_mut_ptr() as *mut i8,
+            width,
+            height,
+            32,
+            0,
+        )
+    }
+}
